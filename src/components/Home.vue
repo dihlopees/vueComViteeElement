4c420557ad721c03a8ae040f48ac2a4febf2da13
Ingrid Rieser Lopes de Oliveira <iroliveira@viptechti.int>
2022-07-26 17:56:13 -0300

    criado alertas

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,22 +4,51 @@
     
     <p> Click para ver alerts: </p>
     
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+    
+    <el-button type="success" @click="mensagemUm">Success</el-button>
+    <el-button type="info" @click="mensagemDois">Info</el-button>
+    <el-button type="warning" @click="mensagemTres">Warning</el-button>
+    <el-button type="danger" @click="mensagemQuatro">Error</el-button>
     
   </div>
 </template>
 
 <script lang="ts">
+import { ElMessage } from "element-plus";
 import { defineComponent } from "vue";
 export default defineComponent({
   setup(): { msg: string } {
     let msg = "Projeto criado com Vite + Element Plus"
 
-    return { msg }
+    const mensagemUm = () => {
+      ElMessage({
+        message: 'Parabens, essa é uma mensagem de sucesso',
+         type: 'success',
+      })
+    }
+
+    const mensagemDois = ():any => {
+      ElMessage({
+        message: 'Essa é uma mensagem de informação',
+         type: 'info',
+      })
+    }
+
+    const mensagemTres = () => {
+      ElMessage({
+        message: 'Ops, essa é uma mensagem de aviso',
+         type: 'warning',
+      })
+    }
+
+    const mensagemQuatro = () => {
+      ElMessage({
+        message: 'Alerta de Erro',
+         type: 'error',
+      })
+    }
+    
+    return { msg, mensagemUm, mensagemDois, mensagemTres, mensagemQuatro }
   },
 });
 </script>
